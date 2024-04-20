ed88579c2fd9e12fd565902e2ef96b7ae7f21028
Shivraj Singh <Shivrajsingh.info.me@gmail.com>
2024-04-20 09:04:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(morgan("tiny"));
 app.disable("x-powered-by"); // less hackers know about our stack
 
-const port = 8080;
+// const port = 8080;
 
 /** HTTP GET Request */
 app.get("/", (req, res) => {
